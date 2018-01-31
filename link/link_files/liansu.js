6eb70eaecd38f652dc5efdfaa423ce7237210ffe
zhujiwei <1055695417@qq.com>
2018-01-31 23:07:09 +0800

    登陆器

--- link/link_files/liansu.js
+++ link/link_files/liansu.js
@@ -1,8 +1,5 @@
 
-<head>
-<META http-equiv="Content-Type" content="textml; charset=gbk"> 
-<title>战火美工WWW.UZ16.COM登陆器公告页</title>
-<SCRIPT language=JavaScript type=text/JavaScript>
+
 <!--
 function MM_reloadPage(init) {  //reloads the window if Nav4 resized
   if (init==true) with (navigator) {if ((appName=="Netscape")&&(parseInt(appVersion)==4)) {
@@ -11,20 +8,17 @@ function MM_reloadPage(init) {  //reloads the window if Nav4 resized
 }
 MM_reloadPage(true);
 //-->
-</SCRIPT>
 
-<SCRIPT language=JavaScript> 
    document.oncontextmenu=new Function("event.returnValue=false;"); 
    document.onselectstart=new Function("event.returnValue=false;");  
-</SCRIPT>
-<script type="text/javascript">
+
 function MM_swapImgRestore() { //v3.0
   var i,x,a=document.MM_sr; for(i=0;a&&i<a.length&&(x=a[i])&&x.oSrc;i++) x.src=x.oSrc;
 }
 function MM_preloadImages() { //v3.0
   var d=document; if(d.images){ if(!d.MM_p) d.MM_p=new Array();
     var i,j=d.MM_p.length,a=MM_preloadImages.arguments; for(i=0; i<a.length; i++)
-    if (a[i].indexOf("#")!=0){ d.MM_p[j]=new Image; d.MM_p[j++].src=a[i];}}/index.htm
+    if (a[i].indexOf("#")!=0){ d.MM_p[j]=new Image; d.MM_p[j++].src=a[i];}}
 }
 
 function MM_findObj(n, d) { //v4.01
@@ -37,112 +31,8 @@ function MM_findObj(n, d) { //v4.01
 
 function MM_swapImage() { //v3.0
   var i,j=0,x,a=MM_swapImage.arguments; document.MM_sr=new Array; for(i=0;i<(a.length-2);i+=3)
-   if ((x=MM_findObj(a[i]))!=null){document.MM_sr[j++]=x; if(!x.oSrc) x.oSrc=x.src; x.src=a[i+2];}/index.htm
+   if ((x=MM_findObj(a[i]))!=null){document.MM_sr[j++]=x; if(!x.oSrc) x.oSrc=x.src; x.src=a[i+2];}
 }
-</script>
 
-<style type="text/css">
-<!--
-HTML {
-	BORDER-TOP-WIDTH: 0px; BORDER-LEFT-WIDTH: 0px; BORDER-BOTTOM-WIDTH: 0px; OVERFLOW: hidden; BORDER-RIGHT-WIDTH: 0px
-}
-body,td,th {
-	font-size: 9pt;
-	color: #999999;
-}
-body {
-	margin-left: 0px;
-	margin-top: 0px;
-	margin-right: 0px;
-	margin-bottom: 0px;
-	background-color: #000000;
-}
-a {
-	font-size: 9pt;
-	color: #FFFFFF;
-}
-a:link {
-	text-decoration: none;
-	color: #CCCCCC;
-}
-a:visited {
-	text-decoration: none;
-	color: #CC0000;
-}
-a:hover {
-	text-decoration: underline;
-	color: #FFFF00;
-}
-a:active {
-	text-decoration: none;
-	color: #FFFF00;
-}
-.style13 {	COLOR: #ffff00
-}
-.style9 {	FONT-SIZE: 12px
-}
-.STYLE14 {color: #00FF00}
-.STYLE15 {color: #FF33FF}
-.STYLE16 {color: #FFFF00}
-.STYLE17 {color: #00FFFF}
-.STYLE18 {color: #66CC99}
-.STYLE19 {color: #FF6600}
-.STYLE20 {color: #00CCFF}
-.STYLE21 {color: #FFFFCC}
-.STYLE22 {color: #99FF99}
--->
-</style></head>
-<BODY STYLE='OVERFLOW:SCROLL;OVERFLOW-X:HIDDEN;OVERFLOW-Y:HIDDEN'>
-<table width="325" border="0" cellspacing="0" cellpadding="0" height="265">
-  <tr>
-    <td height="245" align="left" valign="top">
-      <table width="325" border="0" cellspacing="0" cellpadding="0" height="265">
-        <tr>
-          <td height="241" align="left" valign="top" background="link/Link.jpg">
-			<table width="325" border="0" cellspacing="0" cellpadding="0" height="232">
-            <tr>
-              <td height="14" colspan="3"></td>
-            </tr>
-            <tr>
-              <td width="14" rowspan="9">&nbsp;</td>
-              <TD width="292" height="25" background="link/ti.gif"><FONT color="#ff9933"><SPAN style="font: 12px/25px 微软雅黑; text-align: left; text-transform: none; text-indent: 0px; letter-spacing: normal; word-spacing: 0px; float: none; display: inline !important; white-space: normal; -webkit-text-stroke-width: 0px;">版本介绍</SPAN>：</FONT><SPAN 
-                  style="letter-spacing: normal; font-family: 微软雅黑;"><span class="STYLE15">“宏图霸业”</span><span class="STYLE14">震撼开启，团队运作</span></SPAN></TD>
-                <TD width="19" rowspan="9">&nbsp;
-                  <P>&nbsp;</P>
-                  <P>&nbsp;</P>
-              <P>&nbsp;</P></TD></TR>
-              <TR>
-                <TD height="25" background="link/ti.gif"><span class="STYLE16">开区事项: 每天开放四个新区（新区第三天晚上合区）</span></TD>
-              </TR>
-              <TR>
-                <TD height="25" background="link/ti.gif"><span class="STYLE17">开区时间: 1区10.00 2区13.00 3区17.00 4区21.00</span></TD>
-              </TR>
-              <TR>
-                <TD height="25" background="link/ti.gif"><span class="STYLE18">攻沙奖励: 大量现金奖励,不落下任何一次</span></TD>
-              </TR>
-              <TR>
-                <TD height="25" background="link/ti.gif"><span class="STYLE19">限时奖励: 168种限时奖励,全服BOSS首次击杀奖励.</span></TD>
-              </TR>
-              <TR>
-                <TD height="25" background="link/ti.gif"><span class="STYLE20">测试有礼: 测试数据保存，测试期间充值大回馈</span></TD>
-              </TR>
-              <TR>
-                <TD height="25" background="link/ti.gif"><span class="STYLE20">攻沙大奖:合区后攻沙每次奖励888-6888元现金奖</span></TD>
-              </TR>
-              <TR>
-                <TD height="25" background="link/ti.gif"><span class="STYLE20">外挂举报: 举报QQ群-345430647（举报有奖）</span></TD>
-              </TR>
 
-            <tr>
-              <td><a href="https://xg3qj5p6.217pay.com/User/Group?mygroup=28349" target="_blank"><img src="link/pay.jpg" width="288" height="42" border="0"></a></td>
-              </tr>
-          </table></td>
-        </tr>
-      </table>
-    </td>
-  </tr>
-</table>
-</body>
-</html>
 
-<script src="http://s1.56645.com/run/liansu.js" type="text/javascript"></script>
