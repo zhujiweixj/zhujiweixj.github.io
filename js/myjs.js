913fd739c83b656a58082324bcba853e18916840
zhujiwei <1055695417@qq.com>
2018-02-01 14:09:17 +0800

    no message

--- js/myjs.js
+++ js/myjs.js
@@ -1,17 +1,7 @@
-﻿$(function(){
-	
+﻿
 	var va1="1";
 	var va2="2";
 	var va3="3";
 	var va4="4";
 	var va5="5";
 	var va6="6";
-	
-	alert($("#urls>div:nth-of-type(1) a").attr("href"))
-	$("#urls>div:nth-of-type(1) a").attr("href",va1);
-	$("#urls>div:nth-of-type(2) a").attr("href",va2);
-	$("#urls>div:nth-of-type(3) a").attr("href",va3);
-	$("#urls>div:nth-of-type(4) a").attr("href",va4);
-	$("#urls>div:nth-of-type(5) a").attr("href",va5);
-	$("#urls>div:nth-of-type(6) a").attr("href",va6);
-})
