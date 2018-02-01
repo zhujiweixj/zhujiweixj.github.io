e84474a483a5912feade163481c5e772fd620824
zhujiwei <1055695417@qq.com>
2018-02-01 14:49:57 +0800

    no message

--- js/myjs.js
+++ js/myjs.js
@@ -6,10 +6,7 @@
 	var va5="5";
 	var va6="6";
 $(function(){
-	alert(va1)
-	alert($("#urls>div:nth-of-type(1) a").attr("href"))
 	$("#urls>div:nth-of-type(1) a").attr("href",va1);
-	alert($("#urls>div:nth-of-type(1) a").attr("href"))
 	$("#urls>div:nth-of-type(2) a").attr("href",va2);
 	$("#urls>div:nth-of-type(3) a").attr("href",va3);
 	$("#urls>div:nth-of-type(4) a").attr("href",va4);
