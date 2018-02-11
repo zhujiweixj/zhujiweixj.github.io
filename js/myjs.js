ccfc3204f83fc9ab99b98bf9847aa17331c60fcb
zhujiwei <1055695417@qq.com>
2018-02-11 10:00:41 +0800

    no message

--- js/myjs.js
+++ js/myjs.js
@@ -6,10 +6,10 @@
 	var va5="5";
 	var va6="6";
 $(function(){
-	$("#urls>div:nth-of-type(1) a").attr("href",va1);
-	$("#urls>div:nth-of-type(2) a").attr("href",va2);
-	$("#urls>div:nth-of-type(3) a").attr("href",v3);
-	$("#urls>div:nth-of-type(4) a").attr("href",va4);
-	$("#urls>div:nth-of-type(5) a").attr("href",va5);
-	$("#urls>div:nth-of-type(6) a").attr("href",va6);
+	$("#shjdxzurl>div:nth-of-type(1) a").attr("href",va1);
+	$("#shjdxzurl>div:nth-of-type(2) a").attr("href",va2);
+	$("#shjdxzurl>div:nth-of-type(3) a").attr("href",v3);
+	$("#shjdxzurl>div:nth-of-type(4) a").attr("href",va4);
+	$("#shjdxzurl>div:nth-of-type(5) a").attr("href",va5);
+	$("#shjdxzurl>div:nth-of-type(6) a").attr("href",va6);
 })
