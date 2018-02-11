849f009547aef46c0dd091e01b0340b1fc87e8cb
zhujiwei <1055695417@qq.com>
2018-02-11 10:04:32 +0800

    no message

--- js/myjs.js
+++ js/myjs.js
@@ -1,10 +1,10 @@
 ﻿
-	var va1="1";
-	var va2="2";
+	var va1="https://pan.baidu.com/s/1bqIL6sJ";
+	var va2="https://share.weiyun.com/c6ed6b08ec987ea1e4d9182ebea2ebb2";
 	var v3="http://193.112.19.144/dlq/shjddlq.rar";
-	var va4="4";
-	var va5="5";
-	var va6="6";
+	var va4="https://pan.baidu.com/s/1i6LrhsD";
+	var va5="https://share.weiyun.com/1fee0cd143604a5f1965aa34453a9763";
+	var va6="http://193.112.19.144/patch/shjdpatch.rar";
 $(function(){
 	$("#shjdxzurl>div:nth-of-type(1) a").attr("href",va1);
 	$("#shjdxzurl>div:nth-of-type(2) a").attr("href",va2);
@@ -13,3 +13,6 @@ $(function(){
 	$("#shjdxzurl>div:nth-of-type(5) a").attr("href",va5);
 	$("#shjdxzurl>div:nth-of-type(6) a").attr("href",va6);
 })
+
+
+
